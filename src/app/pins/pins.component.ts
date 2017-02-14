0c484586235e3cf3d8d9baad2482504c9706e6d6
Jayadeep KM <kmjayadeep@gmail.com>
2017-02-14 18:27:31 +0530

    added masonry, bad hack

--- src/app/pins/pins.component.ts
+++ src/app/pins/pins.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import {PinService} from '../services/pin.service'
+import { Component, OnInit,AfterViewInit } from '@angular/core';
+import {PinService} from '../services/pin.service';
+
+declare var Masonry
 
 @Component({
 	selector: 'app-pins',
 	templateUrl: './pins.component.html',
 	styleUrls: ['./pins.component.css']
 })
-export class PinsComponent implements OnInit {
+export class PinsComponent implements OnInit{
 
 	pins:any = []
 
@@ -16,7 +18,19 @@ export class PinsComponent implements OnInit {
 		this.pinService.getAllPins().then(pins=>{
 			this.pins = pins
 			console.log(pins)
+			setTimeout(this.initMasonry,1000)
 		})
+	}
+
+	initMasonry(){
+		var elem = document.querySelector('.grid');
+		console.log(elem)
+		var msnry = new Masonry( elem, {
+		  // options
+		  itemSelector: '.grid-item',
+		  columnWidth: 200
+		});
+		console.log(msnry)
 	}
 
 }
